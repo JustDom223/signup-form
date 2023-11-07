28426f85a32ab4aa56a4e843b4e2eb7bb26c8b49
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2023-11-07 17:03:46 +0100

    Fixed the password validation need to add more parameters

--- script.js
+++ script.js
@@ -2,15 +2,26 @@ const firstNameElement = document.getElementById('first')
 const lastNameElement = document.getElementById('last')
 const form = document.getElementById('form1')
 const errorElement = document.getElementById('error')
+const pwElement = document.getElementById('pw')
+const cpwElement = document.getElementById('cpw')
+
 
 form.addEventListener('submit', (e) => {
     let messages = []
-    if(first.value === '' || first.value === nul){
-        messages.push('Name is required')
+    if(pwElement.value !== cpwElement.value){
+        messages.push('You password does not match')
+        pwElement.style.backgroundColor = 'rgb(253, 231, 231)'
+        // pwElement.style.outline = 'solid 1px  red;'
+        cpwElement.style.backgroundColor = 'rgb(253, 231, 231)'
+        // cpwElement.style.outline = 'solid 1px  red;'
+
     }
 
     if(messages.length > 0){
         e.preventDefault()
         errorElement.innerText = messages.join(', ')
     }
+
 })
+
+
